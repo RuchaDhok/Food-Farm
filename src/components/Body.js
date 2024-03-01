3b3835e065de05d1cc43e0759c198af645c4e258
RuchaDhok <ruchadhok28july@gmail.com>
2024-03-01 22:05:31 +0530

    session 6 done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
+import { SWIGGY_API } from "../utils/constant";
 //import resList from "../utils/mockData";
 
 const Body = () => {
@@ -14,9 +15,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const data = await fetch(SWIGGY_API);
     const json = await data.json();
     const restroInfo =
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/gbllr6pmzare1fdc0ayt";
+
+export const SWIGGY_API =
+  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9351929%26lng%3D77.62448069999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
